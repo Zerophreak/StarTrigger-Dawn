32c7b36f10444f6562a2fefdfbf6fce8855a2f9e
Raymon_Rutten <80789205+Zerophreak@users.noreply.github.com>
2026-01-14 14:28:02 +0100

    adding player script

--- Scenes/Player/Player.cs
+++ Scenes/Player/Player.cs
@@ -3,38 +3,27 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const string GroupName = "player";
+
+	private enum PlayerState { Idle, Hurt, Fly }
+
+	 private float _speed = 300f;
+	 private float _rotationspeed = 150.0f;
+	 private bool _invincible = false; 
+
+	[Export] private Sprite2D _sprite;
+
+
+	public override void _Ready()
+	{
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
+	}
 
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
-
-		Velocity = velocity;
-		MoveAndSlide();
+	private void GetInput()
+	{
+		
 	}
 }
