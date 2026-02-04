b3e13c5c5538bb7ced5a64097b463bd6bb117b6b
Raymon_Rutten <80789205+Zerophreak@users.noreply.github.com>
2026-02-04 14:07:42 +0100

    Update of player
    control update for player

--- Scenes/Player/Player.cs
+++ Scenes/Player/Player.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+// Signed: {_Outcast 0-}
 
 public partial class Player : CharacterBody2D
 {
@@ -7,8 +8,9 @@ public partial class Player : CharacterBody2D
 
 	private enum PlayerState { Idle, Hurt, Fly }
 
-	 private float _speed = 300f;
-	 private float _rotationspeed = 150.0f;
+	 private float _speed = 150.0f;
+	 private float _rotationSpeed = 1.5f;
+	 private float _rotationDirection;
 	 private bool _invincible = false; 
 
 	[Export] private Sprite2D _sprite;
@@ -20,10 +22,14 @@ public partial class Player : CharacterBody2D
 
 	public override void _PhysicsProcess(double delta)
 	{
+		GetInput();
+		Rotation += _rotationDirection * _rotationSpeed * (float)delta;
+		MoveAndSlide();
 	}
 
 	private void GetInput()
 	{
-		
+		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
+		Velocity = inputDirection * _speed;
 	}
 }
